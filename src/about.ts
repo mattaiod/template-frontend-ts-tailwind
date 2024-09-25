ccbbb9990a5f94f52cea9083884a26e850772c81
Matteo Delandhuy <matteodelandhuypro@outlook.fr>
2024-09-25 14:57:33 +0200

    patch mount dist + patch error postcss

--- src/about.ts
+++ src/about.ts
@@ -1,5 +1,6 @@
 // Ajoute un écouteur d'événement sur le bouton "Retour"
 const backButton = document.getElementById("backButton");
+console.log(backButton);
 if (backButton) {
   backButton.addEventListener("click", () => {
     window.location.href = "index.html";
